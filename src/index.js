e487bebcb3551ad145e34c092fb50448277c30ca
Will Hinchman <willhinchman@Wills-MBP.wowway.com>
2018-10-13 02:01:31 -0400

    trying to print

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("something is happening?")
+const escpos = require('escpos');
+ 
+// Select the adapter based on your printer type
+const device  = new escpos.USB();
+// const device  = new escpos.Network('localhost');
+// const device  = new escpos.Serial('/dev/usb/lp0');
+ 
+const options = { encoding: "GB18030" /* default */ }
+// encoding is optional
+ 
+const printer = new escpos.Printer(device, options);
+ 
+ console.log("trying to print")
+ 
+device.open(function(){
+  printer
+  .font('a')
+  .align('ct')
+  .style('bu')
+  .size(1, 1)
+  .text('The quick brown fox jumps over the lazy dog')
+  .text('敏捷的棕色狐狸跳过懒狗')
+  .barcode('1234567', 'EAN8')
+  .qrimage('https://github.com/song940/node-escpos', function(err){
+    this.cut();
+    this.close();
+  });
+});
