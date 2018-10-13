6b65204407c264d05e9acb0ad514121aac0e003c
Will Hinchman <willhinchman@Wills-MBP.wowway.com>
2018-10-13 02:35:26 -0400

    testing LP printing

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 var fs = require('fs');
 
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+
 fs.writeFile("/tmp/test", "Hey there!\nIs this Printing?\nOMG!!!", function(err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log("The file was saved!");
+	console.log("The file was saved!");
+	dir = exec("lp -d ZJ-58 ./tmp/test", function(err, stdout, stderr) {
+		if (err) {
+		  // should have err.code here?  
+		}
+		console.log(stdout);
+	  });
+	  
+	  dir.on('exit', function (code) {
+		// exit code is code
+	  });
+	  
 }); 
 
 
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-dir = exec("lp -d ZJ-58 ./tmp/test", function(err, stdout, stderr) {
-	if (err) {
-	  // should have err.code here?  
-	}
-	console.log(stdout);
-  });
-  
-  dir.on('exit', function (code) {
-	// exit code is code
-  });
+
+
 
 
