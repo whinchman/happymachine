c0b23be1f8efaf299ef24656b9e83d8a6091d582
Will Hinchman <will.hinchman@gmail.com>
2018-10-13 21:07:32 -0400

    print from python?

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ try:
         GPIO.output(11, GPIO.LOW)
 except KeyboardInterrupt:
     GPIO.cleanup()
+    f = open("temp.txt", "w")
+    f.write("printing from python maybe?")
+    os.system("lp -d ZJ-58 test.txt")
