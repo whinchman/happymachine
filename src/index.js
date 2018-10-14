11285e71c8c5ce1d87473c340b697d660bbf6277
Will Hinchman <will.hinchman@gmail.com>
2018-10-13 20:47:59 -0400

    dropping while

--- src/index.js
+++ src/index.js
@@ -12,36 +12,36 @@ const sleep = (milliseconds) => {
 function setLED(output, on) {
 
 	if (on === true) {
-		output.write(gpio.HIGH)	
+		output.write(gpio.HIGH)
 	} else {
 		output.write(gpio.LOW)
 	}
-	
+
 }
 
 function sendToPrinter(message) {
 	//have to write to a temp file, since we're going to call out directly to LPS
-	fs.writeFile("/tmp/test", "Hey there!\nIs this Printing?\nOMG!!!", function(err) {
-		if(err) {
+	fs.writeFile("/tmp/test", "Hey there!\nIs this Printing?\nOMG!!!", function (err) {
+		if (err) {
 			return console.log(err);
 		}
-	
+
 		console.log("The file was saved!");
 
 		//call directly out to LPS, not the best solution, but this is a dang raspi we're talking about.
-		dir = exec("lp -d ZJ-58 /tmp/test", function(err, stdout, stderr) {
+		dir = exec("lp -d ZJ-58 /tmp/test", function (err, stdout, stderr) {
 			if (err) {
 				console.log("OH NO")
 				console.log(err)
 			}
 			console.log(stdout);
-		  });
-		  
-		  dir.on('exit', function (code) {
+		});
+
+		dir.on('exit', function (code) {
 			// exit code is code
 			console.log("Should have printed to printer")
-		  });
-	}); 
+		});
+	});
 }
 
 console.log('entering init')
@@ -50,22 +50,19 @@ raspi.init(() => {
 	//   pin: 'P1-3',
 	//   pullResistor: gpio.PULL_UP
 	// });
-   
+
 	const output = new gpio.DigitalOutput('P1-11');
-   
+
 	output.write(gpio.LOW);
 });
 
 var ledOn = false
-console.log('entering while')
-while (true) {
-	console.log('in while')
-	sleep(500).then(() => {
-		console.log('SLEPT')
-		setLED(output, ledOn)
-		ledOn = !ledOn
-	});
-}
+console.log('going sleep')
+sleep(500).then(() => {
+	console.log('SLEPT')
+	setLED(output, ledOn)
+	ledOn = !ledOn
+});
 
 
 
@@ -73,4 +70,4 @@ while (true) {
 
 
 
- 
+
