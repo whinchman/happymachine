dee94bf07c0df4bbebf304e1754d2c73028fd7cf
Will Hinchman <will.hinchman@gmail.com>
2018-10-20 02:20:05 -0400

    Add Red Button

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import datetime
 import json
 import random
 
+# LED defines
 YELLOW_LED = 11
 GREEN_LED = 15
 BLUE_LED = 18
+RED_LED = 21
+
+#button defines
 YELLOW_BUTTON = 16
 GREEN_BUTTON = 13
 BLUE_BUTTON = 19
+RED_BUTTON = 22
 
 random.seed()
 
@@ -59,31 +64,42 @@ def blue_button(channel):
     printMessage("BLUE BUTTON")
     flashLED(BLUE_LED)
 
+def red_button(channel):
+    printMessage("RED BUTTON")
+    flashLED(RED_LED)
+
 print("setting up GPIO")
 
 try:
+    # use the board count pinout. 
     GPIO.setmode(GPIO.BOARD)
-
 
     # setup output pins
     GPIO.setup(YELLOW_LED, GPIO.OUT)
     GPIO.setup(GREEN_LED, GPIO.OUT)
     GPIO.setup(BLUE_LED, GPIO.OUT)
+    GPIO.setup(RED_LED, GPIO.OUT)
 
+    #turn on all LEDS (low grounds out the 5v channel)
     GPIO.output(YELLOW_LED, GPIO.LOW)
     GPIO.output(GREEN_LED, GPIO.LOW)
     GPIO.output(BLUE_LED, GPIO.LOW)
+    GPIO.output(RED_LED, GPIO.LOW)
 
 
     # setup input pins
     GPIO.setup(YELLOW_BUTTON, GPIO.IN)
     GPIO.setup(GREEN_BUTTON, GPIO.IN)
     GPIO.setup(BLUE_BUTTON , GPIO.IN)
+    GPIO.setup(RED_BUTTON , GPIO.IN)
 
+    # set callbacks
     GPIO.add_event_detect(GREEN_BUTTON, GPIO.FALLING, callback=yellow_button, bouncetime=3000)
     GPIO.add_event_detect(YELLOW_BUTTON, GPIO.FALLING, callback=green_button, bouncetime=3000)
     GPIO.add_event_detect(BLUE_BUTTON, GPIO.FALLING, callback=blue_button, bouncetime=3000)
-    
+    GPIO.add_event_detect(RED_BUTTON, GPIO.FALLING, callback=blue_button, bouncetime=3000)
+
+    # RUN LOOP
     message = raw_input('\nPress any key to exit.\n')
 
 except:
