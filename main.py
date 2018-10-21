e410da61c86171f7f1404c0d70c5debaffcebec7
Will Hinchman <will.hinchman@gmail.com>
2018-10-21 04:24:36 -0400

    formmating

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def green_button(channel):
     numbers.append(random.randint(1,25))
     numbersString = "Lucky Numbers: %d %d %d %d %d %d " % (numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5])
     
-    message = fortunes[randIndex] + "\n" + numbersString
+    message = fortunes[randIndex] + "\n\n" + numbersString
     printMessage(message)
     flashLED(GREEN_LED)
 
